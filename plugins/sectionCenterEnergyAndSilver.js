7395b2f571b375fcde6e6a193fa037b15cebaa6c
fromddy <ddy_mainland@163.com>
2022-06-20 13:08:43 +0800

    update center E&S

--- plugins/sectionCenterEnergyAndSilver.js
+++ plugins/sectionCenterEnergyAndSilver.js
@@ -10,7 +10,7 @@ import { waitForMoveOnchain } from "./logicForMove";
 import { isNoOwner, isOther } from "./logicForAccount";
 import { getJunkEnabled, getMyJunkBudget } from "./logicForJunk";
 import { sectionAbandon } from "./sectionAbandon";
-import { colorForInfo, ENERGY_TARGET_PLANETS, ENERGY_SOURCE_PLANETS, colorForWarn } from './cfgForColor';
+import { colorForInfo, ENERGY_TARGET_PLANETS, ENERGY_SOURCE_PLANETS, colorForWarn,colorForError } from './cfgForColor';
 import { canCapture } from "./logicForInvadeAndCapture";
 import { artifactFilter } from "./logicForArtifactState";
 
@@ -74,7 +74,7 @@ function sourceFilter(planet) {
 
 
 
-export async function selectedToCenterEnergyAndSilver(setInfo) {
+export async function selectedToCenterEnergyAndSilver(setInfo,mode) {
     let p = ui.getSelectedPlanet();
     let content, itemInfo;
     if (p === undefined) {
@@ -83,17 +83,26 @@ export async function selectedToCenterEnergyAndSilver(setInfo) {
         addToLog(itemInfo, setInfo);
         return;
     }
-    await sectionCenterEnergyAndSilver(setInfo, p);
+    await sectionCenterEnergyAndSilver(setInfo,p,1,mode);
 }
 
 
-export async function sectionCenterEnergyAndSilver(setInfo, targetPlanet = undefined, maxTargetNumber = 6) {
+// mode === 1 sorted mainly by energy landing 
+// mode === 2 sorted mainly by time 
+export async function sectionCenterEnergyAndSilver(setInfo, targetPlanet = undefined, maxTargetNumber = 6,mode =1) {
 
     beginSection('[CE&S] === center energy & silver begin ===', setInfo);
+    if(mode === 1){
+        beginSection('[CE&S] sorted mainly by energyLanding',setInfo);
+    }else {
+        beginSection('[CE&S] sorted mainly by time',setInfo);
+
+    }
 
     drawSign = true;
     showInit();
     let content, onClick, itemInfo;
+  
 
     content = '[CE&S] NOTICE: not consider the energy growth';
     itemInfo = greenInfo(content);
@@ -190,9 +199,14 @@ export async function sectionCenterEnergyAndSilver(setInfo, targetPlanet = undef
                 let aEnergyLanding = df.getEnergyArrivingForMove(a.locationId, to.locationId, aDist, aEnergy);
                 let bEnergyLanding = df.getEnergyArrivingForMove(b.locationId, to.locationId, bDist, bEnergy);
 
-                if (aEnergyLanding !== b.energyLanding) return bEnergyLanding - aEnergyLanding;
-                else return aTime - bTime;
-
+                if(mode === 1){
+                    if (aEnergyLanding !== b.energyLanding) return bEnergyLanding - aEnergyLanding;
+                    else return aTime - bTime;
+                }else if(mode === 2){
+                    if(aTime!==bTime) return aTime - bTime;
+                    else return bEnergyLanding - aEnergyLanding;
+                }
+              
             });
 
 
